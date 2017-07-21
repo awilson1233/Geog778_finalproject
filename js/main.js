8e0e54dead63d429f90991d5b46072e33caf4a20
Angela Wilson <wilson38@wisc.edu>
2017-07-21 16:34:33 -0700

    load layers onto map

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ var admin2Style = {
 			fillOpacity: 0.3
 		};
 //call in country boundary layers, do this first so the other layers are placed on top
-  $.ajax("data/admin1.geojson", {
+var admin0 = $.ajax("data/admin0.geojson", {
        dataType: "json",
        success: function(response){
             //create a Leaflet GeoJSON layer and add it to the map
@@ -36,7 +36,16 @@ var admin2Style = {
 
         }
     });
-   $.ajax("data/admin2.geojson", {
+
+var admin1 = $.ajax("data/admin1.geojson", {
+       dataType: "json",
+       success: function(response){
+            //create a Leaflet GeoJSON layer and add it to the map
+           L.geoJson(response, {style: admin1Style}).addTo(map);
+
+        }
+    });
+var admin2 = $.ajax("data/admin2.geojson", {
         dataType: "json",
         success: function(response){
             //create a Leaflet GeoJSON layer and add it to the map
@@ -44,3 +53,44 @@ var admin2Style = {
 
         }
     });
+
+var disputedAreas = $.ajax("data/disputedareas.geojson", {
+        dataType: "json",
+        success: function(response){
+            //create a Leaflet GeoJSON layer and add it to the map
+            L.geoJson(response).addTo(map);
+
+        }
+    });
+
+var humanitarianAssistance = $.ajax("data/humanitarianAssistance.geojson", {
+        dataType: "json",
+        success: function(response){
+            //create a Leaflet GeoJSON layer and add it to the map
+            L.geoJson(response).addTo(map);
+
+        }
+    });
+
+var foodSecurity = $.ajax("data/foodSecurity.geojson", {
+        dataType: "json",
+        success: function(response){
+            //create a Leaflet GeoJSON layer and add it to the map
+            L.geoJson(response).addTo(map);
+
+        }
+    });
+
+var livelihoodZones = $.ajax("data/livelihoodzones.geojson", {
+        dataType: "json",
+        success: function(response){
+            //create a Leaflet GeoJSON layer and add it to the map
+            L.geoJson(response).addTo(map);
+
+        }
+    });
+var layerToggle = {'South Sudan Boundary': admin0, 'States': admin1, 'Counties': admin2, 'Disputed Areas': disputedAreas, 'Humanitarian Assistance': humanitarianAssistance, 'Food Security': foodSecurity, 'Livelihood Zones': livelihoodZones};
+
+
+//This adds the layer control to your map.
+L.control.layers(layerToggle).addTo(map);
