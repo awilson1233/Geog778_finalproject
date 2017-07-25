20bc215ef12f7e743bd7a8120786180f02493899
Angela Wilson <wilson38@wisc.edu>
2017-07-24 19:42:29 -0700

    layer styling

--- js/main.js
+++ js/main.js
@@ -29,11 +29,54 @@ var admin2Style = {
 			fillOpacity: 0.3
 		};
 
-// var humanitarianAssistanceStyle = {
-//         switch (feature.properties.HA2) {
-//             case '1': return {color: "#ff0000"};
-//             case '0':   return {color: "#0000ff"};
-        // }
+var disputedAreasStyle = {
+			fillColor: "#ff0000",
+			weight: 2,
+			opacity: 0.4,
+			color: 'red',
+			fillOpacity: 0.3
+		};
+
+
+function foodSecurityColor(d) {
+    return d == 5  ? '#5C1010' :
+           d == 4  ? '#FF0000' :
+           d == 3  ? '#FF8000' :
+           d == 2  ? '#FFFF33' :
+           d == 1  ? '#E5FFCC' :
+                     '#FFEDA0';
+}
+
+function foodSecurityStyle(feature) {
+    return {
+        fillColor: foodSecurityColor(feature.properties.ML2),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        // dashArray: '3',
+        fillOpacity: 0.7
+    };
+}
+
+
+
+function humanitarianAssistanceColor(h) {
+    return h == 1.0  ? '#5C1010' :
+                     '#FFEDA0';
+}
+function humanitarianAssistanceStyle(feature) {
+    return {
+        fillColor: humanitarianAssistanceColor(feature.properties.HA2),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        // dashArray: '3',
+        fillOpacity: 0.7
+    };
+}
+
+var stripes = new L.StripePattern({ fillPattern: stripes, fillOpacity: 1.0});
+
 
 //call in country boundary layers, do this first so the other layers are placed on top
 var admin0 = $.ajax("data/admin0.geojson", {
@@ -65,58 +108,53 @@ var admin2 = $.ajax("data/admin2.geojson", {
 var disputedAreas = $.ajax("data/disputedareas.geojson", {
         dataType: "json",
         success: function(response){
+          console.log(response);
             //create a Leaflet GeoJSON layer and add it to the map
-            L.geoJson(response).addTo(map);
-
+            L.geoJson(response, {style: stripes}).addTo(map);
+            console.log(stripes);
         }
     });
 
-var humanitarianAssistance = $.ajax("data/humanitarianAssistance.geojson", {
+var humanitarianAssistance = $.ajax("data/humanitarianAssistance2.geojson", {
         dataType: "json",
         success: function(response){
+          console.log(response);
             //create a Leaflet GeoJSON layer and add it to the map
-            L.geoJson(response, {
-                style: function(feature) {
-                    switch (feature.properties.HA2) {
-                        case '1': return {color: "#ff0000"};
-                        case '0':   return {color: "#999"};
-                    }
-                  }
-                }).addTo(map);
+            L.geoJson(response, {style: humanitarianAssistanceStyle}).addTo(map);
 
         }
     });
 
-var foodSecurity = $.ajax("data/foodSecurity.geojson", {
-        dataType: "json",
-        success: function(response){
-            //create a Leaflet GeoJSON layer and add it to the map
-            L.geoJson(response).addTo(map);
+// var foodSecurity = $.ajax("data/foodSecurity.geojson", {
+//         dataType: "json",
+//         success: function(response){
+//             //create a Leaflet GeoJSON layer and add it to the map
+    //         L.geoJson(response, {style: foodSecurityStyle}).addTo(map);
+    //
+    //     }
+    // });
 
-        }
-    });
-
-var livelihoodZones = $.ajax("data/livelihoodzones.geojson", {
-        dataType: "json",
-        success: function(response){
-            //create a Leaflet GeoJSON layer and add it to the map
-            L.geoJson(response).addTo(map);
-
-        }
-    });
-var layerToggle = {
-  'South Sudan Boundary': admin0,
-  'States': admin1,
-  'Counties': admin2,
-  'Disputed Areas': disputedAreas,
-  'Humanitarian Assistance': humanitarianAssistance,
-  'Food Security': foodSecurity,
-  'Livelihood Zones': livelihoodZones
-};
-
-console.log(layerToggle)
+// var livelihoodZones = $.ajax("data/livelihoodzones.geojson", {
+//         dataType: "json",
+//         success: function(response){
+//             //create a Leaflet GeoJSON layer and add it to the map
+//             L.geoJson(response).addTo(map);
+//
+//         }
+//     });
+// var layerToggle = {
+//   'South Sudan Boundary': admin0,
+//   'States': admin1,
+//   'Counties': admin2,
+//   'Disputed Areas': disputedAreas,
+// 'Humanitarian Assistance': humanitarianAssistance,
+// 'Food Security': foodSecurity,
+// 'Livelihood Zones': livelihoodZones
+// };
+//
+// console.log(layerToggle)
 
 //This adds the layer control to your map.
-L.control(layerToggle).addTo(map);
+// L.control.layers(layerToggle).addTo(map);
 
-console.log(layerToggle, map)
+// console.log(layerToggle, map)
